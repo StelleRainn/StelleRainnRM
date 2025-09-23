a3cde3113d3fc695a9227fab4170d02f0dab9847
StelleRainn R.M. <3298257615@qq.com>
2025-09-24 01:13:12 +0800

    初步配置项目,整理目录结构,演示毛玻璃header

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import GeneralHeader from './components/GeneralHeader.vue'
+</script>
+
+<template>
+  <GeneralHeader>这是父组件内容</GeneralHeader>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/GeneralHeader.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <header><slot>default</slot></header>
+</template>
+
+<style scoped lang="scss"></style>
